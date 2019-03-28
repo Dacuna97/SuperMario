321e1811deaac2eb3c319eb14bdc2e80e34a2165
Dacuna97 <diegoacunaberger@gmail.com>
2019-03-28 14:14:06 +0100

    solved movement when dies because of falling

--- practica3.js
+++ practica3.js
@@ -89,7 +89,8 @@ var game = function () {
                         //if dies
                         if (this.p.y > 580) {
                             this.play("die");
-                            this.del("2d");
+                            this.p.dead = true;
+                            this.del("platformerControls");
                             Q.stageScene("endGame", 1, {
                                 label: "You Died"
                             });
@@ -101,6 +102,10 @@ var game = function () {
                     } else {
                         this.play("stand_" + this.p.direction);
                     }
+                }
+                else {
+                    this.p.vy = 0;
+                    this.p.vx = 0;
                 }
 
             }
@@ -199,11 +204,31 @@ var game = function () {
 
         //************************************** */
         Q.scene("endGame", function (stage) {
-            var label = stage.insert(new Q.UI.Text({
+            var container = stage.insert(new Q.UI.Container({
                 x: Q.width / 2,
                 y: Q.height / 2,
+                fill: "rgba(0,0,0,0.5)"
+            }));
+
+            var button = container.insert(new Q.UI.Button({
+                x: 0,
+                y: 0,
+                fill: "#CCCCCC",
+                label: "Play Again"
+            }));
+
+            var label = container.insert(new Q.UI.Text({
+                x: 10,
+                y: -10 - button.p.h,
                 label: stage.options.label
             }));
+
+            button.on("click", function () {
+                Q.clearStages();
+                Q.stageScene('level1');
+            });
+
+            container.fit(20);
         });
 
 
